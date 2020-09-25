34baf858b222b03fae0776a52be41459c38952e1
Rinkal Patadiya <30933361+rinkal-16@users.noreply.github.com>
2020-09-25 14:56:43 +0530

    deploy heroku cli

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/organicApp'));
  
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/organicApp/src/index.html'));
+res.sendFile(path.join(__dirname+'/organicApp/index.html'));
 });
  
 // Start the app by listening on the default Heroku port
