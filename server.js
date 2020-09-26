a5f12bfc49766c3229ef492a68b20477d397b8a7
Rinkal Patadiya <30933361+rinkal-16@users.noreply.github.com>
2020-09-27 01:44:05 +0530

    heroku

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 //Install express server
 const express = require('express');
 const path = require('path');
- 
+
 const app = express();
- 
-// Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/dist'));
- 
+
+// Serve only the static files form the dist directory
+//app.use(express.static('./dist/organictempapp'));
+app.use(express.static(__dirname + '/dist/organic-app'));
+
 app.get('/*', function(req,res) {
- 
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+   
+res.sendFile(path.join(__dirname,'/dist/organic-app/index.html'));
 });
- 
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
