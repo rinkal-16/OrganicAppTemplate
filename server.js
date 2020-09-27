8c80de6244d88ba7f18bb7b0a6e228a5d269f715
Rinkal Patadiya <30933361+rinkal-16@users.noreply.github.com>
2020-09-27 15:38:35 +0530

    make it better

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/organic-app'));
 
 app.get('/*', function(req,res) {
    
-res.sendFile(path.join(__dirname,'/organic-app/src/index.html'));
+res.sendFile(path.join(__dirname,'/organic-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
