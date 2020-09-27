24a433644d0107cf0c6470f14092999bbbbb76a2
Rinkal Patadiya <30933361+rinkal-16@users.noreply.github.com>
2020-09-27 10:39:31 +0530

    heroku...

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const compression = require('compression');
 //Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
+app.use(compression());
 
 // Serve only the static files form the dist directory
 //app.use(express.static('./dist/organictempapp'));
