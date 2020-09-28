472301b7db0a0157d1135ba378da578835e32342
Rinkal Patadiya <30933361+rinkal-16@users.noreply.github.com>
2020-09-28 18:12:51 +0530

    Worked on login/register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,23 @@ import { ProductsComponent } from './products/products.component';
 import { BlogComponent } from './blog/blog.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { ContactComponent } from './contact/contact.component';
-
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { AuthGuard } from './auth.guard';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
-  { path: 'products', component: ProductsComponent},
-  { path: 'blog', component: BlogComponent},
+  { path: 'products', component: ProductsComponent, canActivate: [AuthGuard]},
+  { path: 'blog', component: BlogComponent, canActivate: [AuthGuard]},
   { path: 'aboutus', component: AboutusComponent},
   { path: 'contact', component: ContactComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'forget-pwd', component: ForgetPasswordComponent},
+  { path: 'reset-pwd', component: ResetPasswordComponent},
   { path: '', redirectTo: 'home', pathMatch: 'full' }
   ];
 
---
+++ src/app/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(sessionStorage.getItem('x-auth')) {
+        return true
+      } else {
+        alert("You are not logged in, please provide login!!")
+        this.router.navigate(["/login"]);
+        return false
+      }
+    //return true;
+  }
+  
+}
